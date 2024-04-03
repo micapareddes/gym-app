36e8e22955616271a295971f68580876b0db590e
micapareddes <micapareddes@gmail.com>
2024-04-03 14:42:32 -0300

    style: :sparkles: Add go back arrow to title header

--- src/components/TitleHeader.tsx
+++ src/components/TitleHeader.tsx
@@ -1,16 +1,37 @@
-import { HStack, Text } from "native-base";
+import { Button, HStack, Icon, Text } from "native-base";
+import ArrowLeft from "@assets/arrow-left.svg"
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native";
 
 type Props = {
     title: string
+    goBack?: boolean
 }
 
-export function TitleHeader({title}: Props) {
+export function TitleHeader({title, goBack=false}: Props) {
+    const navigation = useNavigation()
+
+    function handleGoBack() {
+        navigation.goBack()
+    }
+
     return(
-        <HStack pt={16} pb={5} px={4} alignItems="center" justifyContent="center">
+        <HStack pt={16} pb={5} px={3} alignItems="center" justifyContent="center">
+            {
+                goBack && (
+                    <TouchableOpacity 
+                        onPress={handleGoBack}
+                    >
+                        <ArrowLeft width={24} height={24} />
+                    </TouchableOpacity>
+                )
+            }
             <Text 
                 color="gray.100"
                 fontFamily="bold"
                 fontSize="xl"
+                flex={1}
+                textAlign="center"
             >{title}</Text>
         </HStack>
     )
