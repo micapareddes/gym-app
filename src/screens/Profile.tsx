19d18a147a80b6f3297053c41811ef3bb40809c0
micapareddes <micapareddes@gmail.com>
2024-04-03 21:00:29 -0300

    style: :zap: Create UI of profile page

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,69 @@
-import { Text } from "native-base";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+import { ProfilePhoto } from "@components/ProfilePhoto";
+import { TitleHeader } from "@components/TitleHeader";
+import { Center, ScrollView, Skeleton, Text, VStack, View } from "native-base";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
+
+const PHOTO_SIZE = 33
 
 export function Profile() {
+    const [photoIsLoading, setPhotoIsLoading] = useState(false)
     return(
-        <Text>Profile</Text>
+        <VStack mb={32}>
+            <TitleHeader title="Perfil" goBack/>
+        <ScrollView>
+            <Center px={4}>
+                {
+                    photoIsLoading ? 
+                        <Skeleton
+                            w={PHOTO_SIZE} 
+                            h={PHOTO_SIZE}
+                            rounded="full"
+                            startColor="gray.300"
+                            endColor="gray.500"
+                        />
+                    : 
+                        <ProfilePhoto 
+                            source={{ uri: 'https://github.com/micapareddes.png' }} 
+                            alt="Foto de perfil do usuário" 
+                            size={PHOTO_SIZE}
+                        />
+                }
+
+                <TouchableOpacity>
+                    <Text color="red.500" fontFamily="bold" fontSize="md" mt={3} mb={8}>
+                        Alterar foto
+                    </Text>
+                </TouchableOpacity>
+
+                <Input placeholder="Nome"/>
+                <Input 
+                    placeholder="E-mail"
+                    value="micaela@gmail.com" 
+                    isDisabled
+                />
+            </Center>
+            <VStack px={7} mt={12} mb={20}>
+                <Text fontFamily="bold" fontSize="md" color="gray.200" mb={4}>
+                    Alterar senha
+                </Text>
+                <Input
+                    placeholder="Senha antiga"
+                    secureTextEntry
+                />
+                <Input
+                    placeholder="Nova Senha"
+                    secureTextEntry
+                />
+                <Input
+                    placeholder="Confirmar nova Senha"
+                    secureTextEntry
+                />
+                <Button>Salvar</Button>
+            </VStack>
+        </ScrollView>
+        </VStack>
     )
 }
