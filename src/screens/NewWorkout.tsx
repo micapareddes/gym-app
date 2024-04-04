0b941ece6e248557616028afc2d78a7b300a96e4
micapareddes <micapareddes@gmail.com>
2024-04-04 19:43:39 -0300

    style: Create UI of New Workout page

--- src/screens/NewWorkout.tsx
+++ src/screens/NewWorkout.tsx
@@ -1,7 +1,36 @@
-import { Text } from "native-base";
+import { HStack, Text, VStack } from "native-base";
+import { useState } from "react";
+
+import { TitleHeader } from "@components/Header/TitleHeader";
+import { Input } from "@components/Input";
+import { TouchableOpacity } from "react-native";
+import { Checkbox } from "@components/Checkbox";
 
 export function NewWorkout() {
+    const [toggleCheckBox, setToggleCheckBox] = useState(false)
     return(
-        <Text>NewWorkout</Text>
+        <>
+            <TitleHeader title="Treino" goBack/>
+            <VStack px={4}>
+                <Text color="gray.200" fontFamily="body" fontSize="sm" mb={4}>
+                    Nome do treino
+                </Text>
+                <Input placeholder="Nome" mb={8}/>
+                <Text color="gray.200" fontFamily="body" fontSize="sm" mb={4}>
+                    Dias da semana
+                </Text>
+                <Input placeholder="Nome" mb={8}/>
+                <Text color="gray.200" fontFamily="body" fontSize="sm" mb={4}>
+                    Excercícios
+                </Text>
+                <Input placeholder="Nome" mb={8}/>
+                <HStack>
+                    <Text color="gray.200" fontFamily="body" fontSize="sm">
+                        Marcar como treino atual
+                    </Text>
+                    <Checkbox />
+                </HStack>
+            </VStack>
+        </>
     )
 }
